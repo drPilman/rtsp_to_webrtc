63cc80f308c3947cab224d18ce1d217da3716d4c
dr.Pilman <doktorpilman@gmail.com>
2022-10-11 22:42:26 +0300

    try to add gstreamer. it didn't work right now

--- server/src/webrtc_impl.rs
+++ server/src/webrtc_impl.rs
@@ -3,7 +3,8 @@ use crate::utils::*;
 use anyhow::{anyhow, Result};
 use serde_json;
 use std::sync::Arc;
-use tokio::net::UdpSocket;
+use tokio::time::{sleep, Duration};
+//use tokio::net::UdpSocket;
 use webrtc::api::interceptor_registry::register_default_interceptors;
 use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_VP8};
 use webrtc::api::APIBuilder;
@@ -16,15 +17,45 @@ use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
 use webrtc::rtp_transceiver::rtp_codec::RTCRtpCodecCapability;
 use webrtc::track::track_local::track_local_static_rtp::TrackLocalStaticRTP;
 use webrtc::track::track_local::{TrackLocal, TrackLocalWriter};
-use webrtc::Error;
+
+use anyhow::Error;
+use derive_more::{Display, Error};
+use futures::executor::block_on;
+use gst::element_error;
+use gst::prelude::*;
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "Missing element {}", _0)]
+struct MissingElement(#[error(not(source))] &'static str);
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
+struct ErrorMessage {
+    src: String,
+    error: String,
+    debug: Option<String>,
+    source: glib::Error,
+}
+
+fn create_pipeline() -> Result<gst::Pipeline, Error> {
+    gst::init()?;
+    let pipeline = gst::parse_launch(
+        "rtspsrc location=rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4 \
+        ! decodebin ! videoconvert ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true \
+        cpu-used=5 deadline=1 ! rtpvp8pay ! appsink name=appsink",
+    )
+    .unwrap();
+    let pipeline = pipeline.dynamic_cast::<gst::Pipeline>().unwrap();
+    Ok(pipeline)
+}
 
 pub async fn new_track<'a>() -> Arc<TrackLocalStaticRTP> {
     let (local_track_chan_tx, mut local_track_chan_rx) =
         tokio::sync::mpsc::channel::<Arc<TrackLocalStaticRTP>>(1);
 
-    let listener = UdpSocket::bind("127.0.0.1:5004")
-        .await
-        .expect("couldn't bind to address");
+    /*let listener = UdpSocket::bind("127.0.0.1:5004")
+    .await
+    .expect("couldn't bind to address");*/
     /*listener
     .connect(source_url)
     .await
@@ -32,6 +63,12 @@ pub async fn new_track<'a>() -> Arc<TrackLocalStaticRTP> {
     //let done_tx3 = done_tx.clone();
 
     tokio::spawn(async move {
+        let pipeline = create_pipeline().unwrap();
+        let sink = pipeline.by_name("appsink").unwrap();
+        let appsink = sink
+            .dynamic_cast::<gst_app::AppSink>()
+            .expect("Sink element is expected to be an appsink!");
+
         let local_track = Arc::new(TrackLocalStaticRTP::new(
             RTCRtpCodecCapability {
                 mime_type: MIME_TYPE_VP8.to_owned(),
@@ -40,20 +77,102 @@ pub async fn new_track<'a>() -> Arc<TrackLocalStaticRTP> {
             "video".to_owned(),
             "webrtc-rs".to_owned(),
         ));
-        let _ = local_track_chan_tx.send(Arc::clone(&local_track)).await;
-        let mut inbound_rtp_packet = vec![0u8; 1600]; // UDP MTU
 
-        while let Ok((n, _)) = listener.recv_from(&mut inbound_rtp_packet).await {
-            if let Err(err) = local_track.write(&inbound_rtp_packet[..n]).await {
-                if Error::ErrClosedPipe == err {
-                    log::debug!("The peerConnection has been closed.");
-                    // The peerConnection has been closed.
-                } else {
-                    log::debug!("video_track write err: {}", err);
+        let _ = local_track_chan_tx.send(Arc::clone(&local_track)).await;
+
+        //let mut inbound_rtp_packet = vec![0u8; 1600];
+
+        //appsink.set_async(true);
+
+        appsink.set_callbacks(
+            gst_app::AppSinkCallbacks::builder()
+                .new_sample(move |appsink| {
+                    let sample = appsink.pull_sample().map_err(|_| gst::FlowError::Eos)?;
+                    let buffer = sample.buffer().ok_or_else(|| {
+                        element_error!(
+                            appsink,
+                            gst::ResourceError::Failed,
+                            ("Failed to get buffer from appsink")
+                        );
+
+                        gst::FlowError::Error
+                    })?;
+
+                    let map = buffer.map_readable().map_err(|_| {
+                        element_error!(
+                            appsink,
+                            gst::ResourceError::Failed,
+                            ("Failed to map buffer readable")
+                        );
+
+                        gst::FlowError::Error
+                    })?;
+                    let inbound_rtp_packet = map.as_slice(); // TODO: move declaration to parent scope
+                                                             // like udp udp listener
+                    log::debug!("{:?}", inbound_rtp_packet.len());
+                    match block_on(local_track.write(&inbound_rtp_packet)) {
+                        Ok(_) => Ok(gst::FlowSuccess::Ok),
+                        Err(_) => Err(gst::FlowError::Error),
+                    }
+                })
+                .build(),
+        );
+
+        // TODO
+        // it's not working. state always Ok but it broke after few microseconds sometimes
+        {
+            let mut state: Result<gst::StateChangeSuccess, gst::StateChangeError> =
+                Err(gst::StateChangeError);
+            for i in 0..10 {
+                state = pipeline.set_state(gst::State::Playing);
+                if state.is_ok() {
+                    log::debug!("connect over rtsp at {i} try");
+                    break;
                 }
-                return;
+                sleep(Duration::from_millis(500)).await;
+            }
+            state
+        }
+        .unwrap();
+
+        /*let bus = pipeline
+            .bus()
+            .expect("Pipeline without bus. Shouldn't happen!");
+
+        // UDP MTU
+
+        for msg in bus.iter_timed(gst::ClockTime::NONE) {
+            use gst::MessageView;
+            match msg.view() {
+                MessageView::Eos(..) => break,
+                MessageView::Error(err) => {
+                    pipeline.set_state(gst::State::Null)?;
+                    return Err(ErrorMessage {
+                        src: msg
+                            .src()
+                            .map(|s| String::from(s.path_string()))
+                            .unwrap_or_else(|| String::from("None")),
+                        error: err.error().to_string(),
+                        debug: err.debug(),
+                        source: err.error(),
+                    }
+                    .into());
+                }
+                _ => (),
             }
         }
+
+        pipeline.set_state(gst::State::Null)?;*/
+
+        /*if let Err(err) = local_track.write(&inbound_rtp_packet[..n]).await {
+            if webrtc::Error::ErrClosedPipe == err {
+                log::debug!("The peerConnection has been closed.");
+                // The peerConnection has been closed.
+            } else {
+                log::debug!("video_track write err: {}", err);
+            }
+            return;
+        }*/
     });
     local_track_chan_rx.recv().await.unwrap()
 }
